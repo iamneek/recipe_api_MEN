6986ff085d90440e3cfc6bf1ede530cfa48939e6
Neek <neeoub@gmail.com>
2025-09-06 13:46:21 +0000

    CRUD works

--- controller/recipe.controller.js
+++ controller/recipe.controller.js
@@ -22,8 +22,33 @@ const addRecipe = async (req, res) => {
     console.log(recipe)
 }
 
+const updateRecipe = async (req, res) => {
+    const new_recipe = req.body
+    try {
+        const updatedRecipe = await recipeModel.findByIdAndUpdate(req.params.id, new_recipe, {
+            new: true
+        })
+        res.status(200).send(updatedRecipe)
+    } catch (err) {
+        console.log("error: ", err);
+        res.status(400).send("Failed")
+    }
+}
+
+const deleteRecipe = async (req, res) => {
+    try {
+        await recipeModel.findByIdAndDelete(req.params.id)
+        res.status(200).send("Requested recipe was deleted.")
+    } catch (err) {
+        console.log("error: ", err);
+        res.status(400).send("Failed")
+    }
+}
+
 module.exports = {
     getAllRecipes,
     addRecipe,
-    getRecipeById
+    getRecipeById,
+    updateRecipe,
+    deleteRecipe
 }
--- route/recipe.route.js
+++ route/recipe.route.js
@@ -2,12 +2,16 @@ const express = require("express")
 const {
     getAllRecipes,
     addRecipe,
-    getRecipeById
+    getRecipeById,
+    updateRecipe,
+    deleteRecipe
 } = require("../controller/recipe.controller")
 const router = express.Router()
 
 router.get("/", getAllRecipes)
 router.post("/add", addRecipe)
 router.get("/:id", getRecipeById)
+router.put("/update/:id", updateRecipe)
+router.delete("/delete/:id", deleteRecipe)
 
 module.exports = router;
